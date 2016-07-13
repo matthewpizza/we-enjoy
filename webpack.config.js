a3426611a8b542f893ae3bea4cd1f84b654613f8
Matthew Spencer <hello@thisismatthew.com>
2016-07-12 20:22:39 -0700

    Build with webpack.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack       = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: {
+    app: [
+      `${__dirname}/scripts/main.js`,
+    ]
+  },
+  output: {
+    path: `${__dirname}/scripts`,
+    filename: '[name].js',
+  },
+  resolve: {
+    alias: {
+      jquery: 'vendor/jquery.min',
+      pjax  : 'vendor/jquery.pjax'
+    },
+    modulesDirectories: ['scripts']
+  },
+  module: {
+    preLoaders: [
+      // pass sourcemaps along
+      {
+        test  : /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+  plugins: [
+    // explicitly expose global variables
+    new webpack.ProvidePlugin({
+      $: 'jquery'
+    })
+  ],
+  // ignore node and node_modules in build
+  target: 'node',
+  externals: [
+    nodeExternals()
+  ],
+  // write sourcemaps to file
+  devtool: '#source-map'
+};
